99ed7db4194f5ae19280828675aa763825b2c68d
rkskekzzz <wkdlfflxh@naver.com>
2023-07-23 01:09:59 +0900

    ✨env 추가

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,32 +3,32 @@ import { Stack } from '../stack'
 const Footer = () => {
   return (
     <footer className='bg-neutral-800 text-neutral-100'>
-      {/* <Stack direction='row' gap='gap-4' className='px-6 py-4'>
+      <Stack direction='row' gap='gap-4' className='px-6 py-4'>
         <section className='max-w-[500px]'>
-          <h3 className='mb-2'>넌버틀</h3>
+          <h3 className='mb-2'>{process.env.NEXT_PUBLIC_COMPANY_NAME}</h3>
           <div className='text-sm text-[#c2c2c2]'>
-            사업자등록번호 0101010
+            {`사업자등록번호 ${process.env.NEXT_PUBLIC_ID}`}
             <span className='mx-[10px] mt-[7px] inline-block h-[10px] w-[1px] bg-[#c2c2c2]' />
-            통신판매업번호 0101010
+            {`대표이사 ${process.env.NEXT_PUBLIC_CEO}`}
             <span className='mx-[10px] mt-[7px] inline-block h-[10px] w-[1px] bg-[#c2c2c2]' />
-            대표이사 0101010
+            {`통신판매업신고번호 ${process.env.NEXT_PUBLIC_COMPANY_MAIL_ORDER_SALES_REGISTRATION_NO}`}
             <span className='mx-[10px] mt-[7px] inline-block h-[10px] w-[1px] bg-[#c2c2c2]' />
-            주소 0101010
+            {`전화번호 ${process.env.NEXT_PUBLIC_PHONE}`}
             <span className='mx-[10px] mt-[7px] inline-block h-[10px] w-[1px] bg-[#c2c2c2]' />
-            이메일 0101010
+            {`이메일 ${process.env.NEXT_PUBLIC_EMAIL}`}
             <span className='mx-[10px] mt-[7px] inline-block h-[10px] w-[1px] bg-[#c2c2c2]' />
-            사업자등록번호 0101010
+            {`주소 ${process.env.NEXT_PUBLIC_ADDRESS}`}
           </div>
         </section>
-        <section>
+        {/* <section>
           <h3 className='mb-2'>소셜</h3>
           <Stack direction='col'>
             <div className='text-sm text-[#c2c2c2]'>instagram /</div>
             <div className='text-sm text-[#c2c2c2]'>facebook /</div>
           </Stack>
-        </section>
+        </section> */}
       </Stack>
-      <section className='border-t-2 border-neutral-200 px-6 py-4'>
+      {/* <section className='border-t-2 border-neutral-200 px-6 py-4'>
         <Stack direction='row' justify='space-between'>
           <div>@nonbuttle</div>
           <Stack gap='gap-4'>
